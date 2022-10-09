8c6bc2670ecc661518709313ee9af1f777c8344e
Jaipriya Kuppaswamy <jai.priya263@gmail.com>
2022-10-10 01:36:27 +0200

    deleted download and added reqs

--- download.py
+++
@@ -1,7 +0,0 @@
-import os
-
-urls = open('https://lua.rlp.de/de/unsere-themen/infektionsschutz/meldedaten-coronavirus/meldedaten-excel/', 'r')
-for i, url in enumerate(urls):
-    path = 'E:\Python\Research Lab\{}'.format(os.path.basename(url)
-    urllib.request.urlretrieve(url, path)
-    
